f1b498490fb10be1ca5579e5da5e20e732dd70cb
prit <prit.sorathiya@gmail.com>
2020-06-17 11:34:49 +0530

    "done messages screen"

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,66 @@
+import 'package:FlutterChat/widgets/category_selector.dart';
+import 'package:FlutterChat/widgets/favorite_contacts.dart';
+import 'package:FlutterChat/widgets/recent_chats.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          iconSize: 30.0,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        elevation: 0,
+        title: Center(
+          child: Text(
+            'Chats',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          CategorySelector(),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).accentColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+              child: Column(
+                children: <Widget>[
+                  FavoriteContacts(),
+                  RecentChats(),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
